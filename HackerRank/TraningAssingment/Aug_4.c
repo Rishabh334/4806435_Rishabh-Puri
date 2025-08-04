e31c25a8616cd0fe004cd88c80b4cabda8d6d0be
Rishabh334 <rishabh06puri@gmail.com>
2025-08-04 23:55:08 +0530

    HackerRank

--- HackerRank/TraningAssingment/Aug_4.c
+++ HackerRank/TraningAssingment/Aug_4.c
@@ -1,44 +1,27 @@
-#include <stdio.h>
-
+#include<stdio.h>
 int main(){
     int day;
-    
-    do {
-        printf("Enter a value from 1 to 7: ");
-        scanf("%d", day);
+    printf("Enter any day between 1 to 7: ");
+   
+    scanf("%d", &day);
 
-        if (day < 1 || day > 7)
-        {
-            printf("That’s not valid. Try a number between 1 and 7.\n");
-        }
-
-    
-    }
-     while (day < 1 || day > 7);
-    switch (day)
-    {
-    case 1:
-        printf("Monday\n");
-        break;
-    case 2:
-        printf("Tuesday!\n");
-        break;
-    case 3:
-        printf("Wednesday!\n");
-        break;
-    case 4:
-        printf("Thursday!\n");
-        break;
-    case 5:
-        printf("Friday!\n");
-        break;
-    case 6:
-        printf("Saturday!\n");
-        break;
-    case 7:
-        printf("Sunday!\n");
-        break;
+    switch (day){
+        case 1: printf("Monday\n");
+                break;
+        case 2: printf("Tuesday\n");
+                break;
+        case 3: printf("Wednesday\n");
+                break;
+        case 4: printf("Thursday\n");
+                break;
+        case 5: printf("Friday\n");
+                break;
+        case 6: printf("Saturday\n");
+                break;
+        case 7: printf("Sunday\n");
+                break;
+        default: printf("Enter Correct choice\n");
     }
 
-    return 0;
+    return 0 ;
 }
